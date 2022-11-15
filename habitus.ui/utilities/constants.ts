8e02e4ddb19efb063d7ec6377935cccee5535198
Flundrahn <flundst@gmail.com>
2022-11-15 01:11:11 +0100

    add deployed url

--- habitus.ui/utilities/constants.ts
+++ habitus.ui/utilities/constants.ts
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'https://localhost:7125/api'
-    : 'TODO put production api url here after deployment';
+    : 'https://habitus-api.azurewebsites.net/api';
 
 export default API_BASE_URL;
